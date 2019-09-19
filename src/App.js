6823ed7aa667455fc2792fbd20c1aa235f7dc391
Jonny Velez <jonny.velez@zalando.de>
2019-09-19 17:16:14 +0200

    change data source, compacted api

--- src/App.js
+++ src/App.js
@@ -53,39 +53,46 @@ class App extends Component {
   componentDidMount() {
     const that = this;
 
-    // fetch('https://wikirate.org/Question_Widget_GHG_emissions.json')
-    fetch('mocked-wikirate-emissions-data.json')
+    fetch('https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=2017')
+    //fetch('mocked-wikirate-emissions-data.json')
       .then(response => response.json())
-      .then(response => { console.log(response); return response; })
-      .then(data => data.items)
-      .then(items => {
-        const itemsByCompany = _.chain(items)
-          .filter(item => item.year === 2017)
-          .groupBy('company')
-          .pickBy((items, companyName) => {
-            const hasScope1 = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 1']);
-            const hasScope2 = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 2']);
-            const hasScope1And2Combined = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 1 and 2 combined']);
-            const containsAllDesiredScopes = hasScope1 && hasScope2 && hasScope1And2Combined;
-            return containsAllDesiredScopes;
-          });
+      // .then(response => { console.log(response); return response; })
+      .then(data => {
+          // const data = response.json();
+          console.log(data);
+          
+          for (const company in data.companies) {
+            console.log(company);
+          }
+      })
+      // .then(items => {
+      //   const itemsByCompany = _.chain(items)
+      //     .filter(item => item.year === 2017)
+      //     .groupBy('company')
+      //     .pickBy((items, companyName) => {
+      //       const hasScope1 = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 1']);
+      //       const hasScope2 = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 2']);
+      //       const hasScope1And2Combined = _.some(items, ['metric', 'Commons+Greenhouse Gas Emissions Scope 1 and 2 combined']);
+      //       const containsAllDesiredScopes = hasScope1 && hasScope2 && hasScope1And2Combined;
+      //       return containsAllDesiredScopes;
+      //     });
 
 
-        const optionsForSuggestDropdownlist = itemsByCompany.map(itemsByOneCompany => ({
-          value: itemsByOneCompany[0].url.split('+')[2],
-          label: itemsByOneCompany[0].company
-        }))
-          .value();
+      //   const optionsForSuggestDropdownlist = itemsByCompany.map(itemsByOneCompany => ({
+      //     value: itemsByOneCompany[0].url.split('+')[2],
+      //     label: itemsByOneCompany[0].company
+      //   }))
+      //     .value();
 
-        const metricsPerCompany = itemsByCompany.map(itemsByOneCompany => ({
-          metrics: itemsByOneCompany,
-          company: itemsByOneCompany[0].url.split('+')[2]
-        }))
-          .value();
-        that.setState({ metricsPerCompany })
-        that.setState({ companyOptions: optionsForSuggestDropdownlist });
-      }
-      )
+      //   const metricsPerCompany = itemsByCompany.map(itemsByOneCompany => ({
+      //     metrics: itemsByOneCompany,
+      //     company: itemsByOneCompany[0].url.split('+')[2]
+      //   }))
+      //     .value();
+      //   that.setState({ metricsPerCompany })
+      //   that.setState({ companyOptions: optionsForSuggestDropdownlist });
+      // }
+      // )
   }
 
   render() {
