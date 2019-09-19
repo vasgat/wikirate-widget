c811c426cf3631fd7cca479c36af0ee369d14108
Jonny Velez <jonny.velez@zalando.de>
2019-09-19 18:58:51 +0200

    tmp commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
       metricsPerCompany: [],
       companyOptions: [],
       selectedCompanyOption: null,
-      chartData: null
+      chartData: null, 
+      foo: null
     }
 
     this.selectedCompanyChangedHandler = this.selectedCompanyChangedHandler.bind(this);
@@ -53,18 +54,93 @@ class App extends Component {
   componentDidMount() {
     const that = this;
 
-    fetch('https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=2017')
-    //fetch('mocked-wikirate-emissions-data.json')
-      .then(response => response.json())
-      // .then(response => { console.log(response); return response; })
-      .then(data => {
-          // const data = response.json();
-          console.log(data);
-          
-          for (const company in data.companies) {
-            console.log(company);
-          }
+    const series = []
+
+    const callYear1 = fetch('https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=2017')
+    const callYear2 = fetch('https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=2018')
+
+    // _.values(callYear2answers])
+
+    Promise.all([callYear1, callYear2])
+      .then(function([dataYear1, dataYear2]) {
+          // console.log('Year1')
+          // console.log(dataYear1)
+
+          // console.log('Year2')
+          // console.log(dataYear2)
+
+          // this.setState({
+          //   foo: dataYear1.json()
+          // });
+
+          const companies = []
+          const series = []
+
+          Promise.all([dataYear1.json(), dataYear2.json()])
+            .then(([jsonYear1, jsonYear2]) => {
+              const answers = _.values(jsonYear2.answers).map(item2 => {
+                const item1 = _.find(_.values(jsonYear1.answers), {"company": item2.company});
+
+                if (item1 == undefined) {
+                  return null
+                }
+                
+                return {
+                  company: item1.company,
+                  valueYear1: parseFloat(item1.value), 
+                  valueYear2: parseFloat(item2.value)
+                }
+              })
+              .filter(item => item != null && !isNaN(item.valueYear1) && !isNaN(item.valueYear2))
+              const sortedAnswers = _.orderBy(answers, ['valueYear2'], ['desc'])
+              series.push({name: '2017', type: 'bar', data: sortedAnswers.map(item => item.valueYear1)})
+              series.push({name: '2018', type: 'bar', data: sortedAnswers.map(item => item.valueYear2)})
+            })
+
+          console.log(series)
+          // dataYear1.json()
+          //   .then(response => 
+          //       response.answers.map(item1 => {
+          //           const item2 = _.findBy(dataYear1.answers, "companyName", item1.companyName);
+          //           return {
+          //               companyName: item1.companyName, 
+          //               valueYear1: item1.value, 
+          //               valueYear2: item2.value
+          //           };
+          //       })
+          //   )
+
+          // dataYear2.json()
+          //   .then(response => {
+          //       _.values(response.answers)
+          //           .forEach(item => answers.push(item))
+          //   })
+
+          // console.log('Answers')
+          // console.log(answers)
+
+          // for (const answer in dataYear2.json()['answers']) {
+          //      console.log(answer)
+          // }
+
+          // dataYear2.then()
       })
+    
+    
+
+    // fetch('https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=2017')
+    // //fetch('mocked-wikirate-emissions-data.json')
+    //   .then(response => response.json())
+    //   // .then(response => { console.log(response); return response; })
+    //   .then(data => {
+    //       // const data = response.json();
+    //       console.log(data);
+    //       const companiesData = []
+
+    //       for (const companyId in data.companies) {
+    //         companiesData
+    //       }
+    //   })
       // .then(items => {
       //   const itemsByCompany = _.chain(items)
       //     .filter(item => item.year === 2017)
@@ -98,6 +174,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+         {this.state.foo}
         <CompanyDropdownList selectedCompanyChangedHandler={this.selectedCompanyChangedHandler} companyOptions={this.state.companyOptions} />
         <EmissionChart chartData={this.state.chartData} />
       </div>
